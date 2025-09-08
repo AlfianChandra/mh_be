facf7282d0f2827ba385879b9452796081e18175
Alfian Chandra <31608738+AlfianChandra@users.noreply.github.com>
2025-09-08 09:07:34 +0700

    -

--- controllers/hint.controller.js
+++ controllers/hint.controller.js
@@ -56,7 +56,6 @@ const hintControllerBuilder = () => {
       //Get and sort DESCENDINGLY
       const hints = await Hint.find({ id_meeting }).sort({
         is_active: -1,
-        createdAt: -1,
       });
       return res.status(200).json({ payload: hints });
     } catch (err) {
