02a1ddbe8107330d7efb82c9ea76e316cf577829
Alfian Chandra <31608738+AlfianChandra@users.noreply.github.com>
2025-09-12 10:24:41 +0700

    -

--- listeners/hint.listener.js
+++ listeners/hint.listener.js
@@ -154,6 +154,7 @@ registry.waitFor("hintns", { timeoutMs: 1000 }).then((io) => {
           console.log(ev.response);
           getKeywords(openai, context, setting)
             .then((keywords) => {
+              console.log(keywords);
               socket.emit("hint:response-complete", {
                 text: ev.response.output[0].content[0].text,
                 keywords: keywords,
