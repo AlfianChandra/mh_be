c1dee39008d0f1bb39090f42f2fd8c8da14ed94d
Alfian Chandra <31608738+AlfianChandra@users.noreply.github.com>
2025-09-22 13:57:37 +0700

    -

--- listeners/hint.listener.js
+++ listeners/hint.listener.js
@@ -129,6 +129,8 @@ registry.waitFor("hintns", { timeoutMs: 1000 }).then((io) => {
           });
         }
 
+        console.log(motion);
+
         input.push({
           role: "user",
           content: `Berikan saya petunjuk tentang narasi ini: ${context}. ${
