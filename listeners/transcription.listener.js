907625566e5a775ce980487c3fa93e5679942f60
Alfian Chandra <31608738+AlfianChandra@users.noreply.github.com>
2025-09-25 11:42:10 +0700

    -

--- listeners/transcription.listener.js
+++ listeners/transcription.listener.js
@@ -198,7 +198,7 @@ async function getOrCreateWs(socketId, lang = "id") {
         // Voice Activity Detection
         turn_detection: {
           type: "server_vad",
-          threshold: 1,
+          threshold: 0.9,
           prefix_padding_ms: 400,
           silence_duration_ms: 150,
           create_response: false,
