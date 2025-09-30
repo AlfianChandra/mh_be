f1d3123bc132d92bf762c79fe35701a3a1f243df
Alfian Chandra <31608738+AlfianChandra@users.noreply.github.com>
2025-09-30 16:27:36 +0700

    -

--- listeners/hint.listener.js
+++ listeners/hint.listener.js
@@ -221,6 +221,13 @@ registry.waitFor("hintns", { timeoutMs: 1000 }).then((io) => {
   });
 });
 
+function cosineSimilarity(vecA, vecB) {
+  const dot = vecA.reduce((sum, a, i) => sum + a * vecB[i], 0);
+  const normA = Math.sqrt(vecA.reduce((sum, a) => sum + a * a, 0));
+  const normB = Math.sqrt(vecB.reduce((sum, b) => sum + b * b, 0));
+  return dot / (normA * normB);
+}
+
 const searchRelevantChunks = async (idMeeting, queryEmbedding, topN = 5) => {
   const allChunks = await FileChunk.find({ id_meeting: idMeeting });
 
